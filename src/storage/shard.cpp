b8afe240c3275c6d5a9c94ae6693a8ed97eb62ae
Chen Zhao <ch3nzhao@gmail.com>
2026-01-26 15:20:26 +0800

    add log

--- src/storage/shard.cpp
+++ src/storage/shard.cpp
@@ -636,7 +636,8 @@ void Shard::WorkOneRound()
         uint64_t delta_us = DurationMicroseconds(ts_);
         if (delta_us >= FLAGS_max_processing_time_microseconds)
         {
-            LOG(ERROR) << "OnReceivedReq cost " << delta_us << "us, cnt = " << i;
+            LOG(ERROR) << "OnReceivedReq cost " << delta_us
+                       << "us, cnt = " << i;
             size_t remaining = nreqs - (i + 1);
             if (remaining > 0)
             {
@@ -664,7 +665,13 @@ void Shard::WorkOneRound()
     }
     req_queue_size_.fetch_sub(processed_reqs, std::memory_order_relaxed);
 
+    int64_t before_submit = butil::cpuwide_time_ns();
     io_mgr_->Submit();
+    auto diff = butil::cpuwide_time_ns() - before_submit;
+    if (diff > 300000)
+    {
+        LOG(INFO) << "IoUring submit cost " << diff << "ns";
+    }
 
     io_mgr_->PollComplete();
     delta_us = DurationMicroseconds(ts_);
