dcdaa1b9d49e687e04d6d7a1e2869d898860f89e
eatbreads <1852611363@qq.com>
2026-01-30 13:20:01 +0800

    wip

--- src/tasks/write_task.cpp
+++ src/tasks/write_task.cpp
@@ -141,6 +141,11 @@ void WriteTask::WritePageCallback(VarPage page, KvError err)
         }
         else
         {
+            LOG(ERROR) << "WritePageCallback releasing index page due to err="
+                       << static_cast<int>(err) << " tbl=" << tbl_ident_
+                       << " page_id=" << idx_page->GetPageId()
+                       << " file_page_id=" << idx_page->GetFilePageId()
+                       << " idx_page=" << idx_page;
             shard->IndexManager()->ReleaseIndexPage(idx_page);
         }
         break;
