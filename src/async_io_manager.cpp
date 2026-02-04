b84919d5f89f3d89482f75c1fdde74445817c90f
Chen Zhao <ch3nzhao@gmail.com>
2026-02-04 15:40:25 +0800

    fix

--- src/async_io_manager.cpp
+++ src/async_io_manager.cpp
@@ -3214,14 +3214,19 @@ KvError CloudStoreMgr::SyncFile(LruFD::Ref fd)
     {
         const TableIdent &tbl_id = *fd.Get()->tbl_->tbl_id_;
         uint64_t term = fd.Get()->term_;
-        KvError err;
-        // For manifest, retry until manifest is uploaded successfully or oss
-        // storage is not enough.
-        do
+        KvError err = UploadFiles(tbl_id, {ToFilename(file_id, term)});
+        if (file_id == LruFD::kManifest)
         {
-            err = UploadFiles(tbl_id, {ToFilename(file_id, term)});
-        } while (err != KvError::NoError && file_id == LruFD::kManifest &&
-                 err != KvError::OssInsufficientStorage);
+            // For manifest, retry until success or error that
+            // means manifest is not uploaded (insufficient storage).
+            while (err != KvError::NoError &&
+                   err != KvError::OssInsufficientStorage)
+            {
+                LOG(WARNING) << "Manifest upload failed with "
+                             << ErrorString(err) << ", retrying.";
+                err = UploadFiles(tbl_id, {ToFilename(file_id, term)});
+            }
+        }
         return err;
     }
 
